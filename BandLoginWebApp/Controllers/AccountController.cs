bfbe0f7b80b575f1da09fa6f1f7bf7f65cc048a3
dixie354 <dheekshithaduri2@gmail.com>
2024-02-25 15:26:51 +0530

    Intial commit

---
+++ BandLoginWebApp/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BandLoginWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+          
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+          
+            return RedirectToAction("Login");
+        }
+    }
+}
